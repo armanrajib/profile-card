81beb31a5feec0b022bbfc20a2f38d0436f07f4c
Arman Rajib <ahrajib6789@gmail.com>
2024-09-25 18:15:29 +0600

    Finish the project 'Profile Card'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const skillList = [
+  {
+    key: 1,
+    name: "HTML",
+    level: "advanced",
+    bgColor: "#CEAB93",
+  },
+  {
+    key: 2,
+    name: "CSS",
+    level: "intermediate",
+    bgColor: "#B9CCED",
+  },
+  {
+    key: 3,
+    name: "JavaScript",
+    level: "advanced",
+    bgColor: "#FF8585",
+  },
+  {
+    key: 4,
+    name: "React",
+    level: "beginner",
+    bgColor: "#D5A4CF",
+  },
+  {
+    key: 5,
+    name: "Python",
+    level: "advanced",
+    bgColor: "#99B080",
+  },
+  {
+    key: 6,
+    name: "Git & GitHub",
+    level: "intermediate",
+    bgColor: "#A58FAA",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Avatar() {
+  return <img className="avatar" src="photo.jpg" alt="Devaloper_Photo" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Arman Rajib</h1>
+      <p>
+        A web developer is a professional who specializes in the creation and
+        maintenance of websites and web applications. They are skilled in
+        programming languages and technologies used for building websites, such
+        as HTML, CSS, JavaScript, and various frameworks and libraries.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skillList.map((skill) => (
+        <Skill skill={skill} key={skill.key} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill }) {
+  return (
+    <div className="skill" style={{ backgroundColor: skill.bgColor }}>
+      <span>{skill.name}</span>
+      <span>
+        {skill.level === "beginner" && "🤓"}
+        {skill.level === "intermediate" && "👌"}
+        {skill.level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
